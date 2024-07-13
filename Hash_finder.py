bc6deec6a1f086ebdf127e85e88579e274d3ca55
MR <motteza1182@gamil.com>
2024-07-13 09:04:41 +0330

    Create Hash_finder.py

---
+++ Hash_finder.py
@@ -0,0 +1,16 @@
+import hashlib
+
+i = 1
+a = "baaa sedaaa ie bi sedaaaa "
+number = int(input("last numbers in hash: "))
+
+while True:
+    a += str(i)
+    i += 1
+    byte_message = bytes(a, 'utf-8')
+    test = hashlib.sha256(byte_message).hexdigest()
+    if number == test[-2:]: 
+        print(test, "this str is true", a)
+        break
+
+a = input ()
